a1fb78bd6a6b581418d8b16c64504b0e78f8d1e1
Nancy Govea <nancy@nancys-mbp.attlocal.net>
2017-11-11 14:53:30 -0600

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,66 +8,82 @@ var losses = 0;
 var guesses = 5;
 var yourGuess = [];
 
+//for computer to pick random letters
+
+var computerguess = options[Math.floor(Math.random()*options.length)];
+
+console.log(computerguess);
+
 //user to key in answer
 document.onkeyup = function(){
 	var userguess = String.fromCharCode(event.keyCode).toLowerCase();
 
-	console.log(userguess);
+	console.log("userguess...",userguess);
+	console.log("computerguess...",computerguess);
 //logs what letters are being selected
 
 	yourGuess.push(userguess);
-
-//for computer to pick random letters
-
-	var computerguess = options[Math.floor(Math.random()*options.length)];
-
-	console.log(computerguess);
 //conditions for games and resets
 
-	if ((userguess === computerguess)){ wins++;
-		}
-		else ((userguess != computerguess))
-		losses++; guesses--;
+	if (userguess === computerguess){ 
+		wins++;
+		computerguess = options[Math.floor(Math.random()*options.length)];
+		// alert("You win!")
+		alert(`You win! u guessed ${userguess}`)
+	}else{
+		guesses--;
+	}
+
 	if (guesses == 0) {
 		losses++;
 		console.log("You lost!");
 		alert("You are not Psychic!");
-		wins = 0;
-		losses = 0;
+		//wins = 0;
+		//losses = 0;
 		guesses = 5;
 		yourGuess = [];
 		computerguess = options[Math.floor(Math.random()* options.length)]
-
-}
-	 if (wins === 2){
-			wins++; 
+	}
+	 
+	if (wins === 2){ 
 //checked that this worked with less options and more guesses
 
 		console.log("you win");
 		alert("You are Psychic!");
+		resetGame();
+	}else if (losses === 2){
+		alert("Game Over!");
+		resetGame();
+
+	}
+	
+	
+	
+	console.log("-----------");
+	console.log("Wins: "+wins);
+	console.log("-----------");
+	console.log("losses: "+losses)
+	console.log("-----------");
+	console.log("guesses left: "+guesses)
+	console.log("-----------");
+	console.log("Guesses so far: "+ yourGuess)
+//show in html
+
+	var html = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses Left: " + guesses + "</p>" + "<p> Your Guesses: " + yourGuess + "</p>";
+
+//show html
+
+	document.querySelector("#game").innerHTML = html;
+
+
+	function resetGame(){
+		console.log("reseting game")
 		wins = 0;
 		losses = 0;
 		guesses = 5;
 		yourGuess = [];
-		computerguess = options[Math.floor(Math.random()*options.length)];}
-	
-	
-	
-		console.log("-----------");
-		console.log("Wins: "+wins);
-		console.log("-----------");
-		console.log("losses: "+losses)
-		console.log("-----------");
-		console.log("guesses left: "+guesses)
-		console.log("-----------");
-		console.log("Guesses so far: "+ yourGuess)
-//show in html
-
-var html = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses Left: " + guesses + "</p>" + "<p> Your Guesses: " + yourGuess + "</p>";
-
-//show html
-
-document.querySelector("#game").innerHTML = html;
+		computerguess = options[Math.floor(Math.random()*options.length)];
+	}
 
 };
 
